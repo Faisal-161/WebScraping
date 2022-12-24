4abe168913980f1015e019efb080357dd693a3eb
Faisal-161 <96625297+Faisal-161@users.noreply.github.com>
2022-12-24 21:22:56 +0600

    Update main.py

--- job site scrapping/main.py
+++ job site scrapping/main.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
+#Can do it by find_all tag in seleimum library
+
 print('Put some skill that you are not familiar with')
 unfamiliar_skill = input('>')
 
